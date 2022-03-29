7b623cd43b7497ae7bc873a01e3c2338c169d2d7
Alexsept177 <alan03002@utb.vaxjo.se>
2022-03-29 15:53:56 +0200

    jobbat med css sida
    har inplementerat 2 css animationer
    och jobbat med index css animationer responsivitet

--- Webbsida/javascript/nav.js
+++ Webbsida/javascript/nav.js
@@ -1,8 +1,11 @@
+const sideNav = document.getElementById("mySidenav");
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  sideNav.style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
+  document.querySelector("header").style.marginLeft = "250px";
 }
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  sideNav.style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
+  document.querySelector("header").style.marginLeft= "0";
 }
