d192e58b84dcec4e79710a9070d84f4723c97443
melvinsjostrand <mesj04001@utb.vaxjo.se>
2022-04-05 14:02:29 +0200

    alla

--- Webbsida/javascript/nav.js
+++ Webbsida/javascript/nav.js
@@ -1,11 +1,7 @@
 const sideNav = document.getElementById("mySidenav");
 function openNav() {
   sideNav.style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-  document.querySelector("header").style.marginLeft = "250px";
 }
 function closeNav() {
   sideNav.style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-  document.querySelector("header").style.marginLeft= "0";
 }
