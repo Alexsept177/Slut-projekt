f0eb38adb42275613710e3a9914d2970bdfa2387
Caspian <alka04006@utb.vaxjo.se>
2022-04-22 10:35:34 +0200

    animation css

---
+++ Webbsida/javascript/choice.js
@@ -0,0 +1,31 @@
+const none = document.querySelectorAll(".del")
+
+
+function showCode1(){
+    none[0].classList.add("show-code")
+    none[1].classList.remove("show-code")
+    none[2].classList.remove("show-code")
+    none[3].classList.remove("show-code")
+}
+
+
+function showCode2(){
+    none[0].classList.remove("show-code")
+    none[1].classList.add("show-code")
+    none[2].classList.remove("show-code")
+    none[3].classList.remove("show-code")
+}
+
+function showCode3(){
+    none[0].classList.remove("show-code")
+    none[1].classList.remove("show-code")
+    none[2].classList.add("show-code")
+    none[3].classList.remove("show-code")
+}
+
+function  showCode4(){
+    none[0].classList.remove("show-code")
+    none[1].classList.remove("show-code")
+    none[2].classList.remove("show-code")
+    none[3].classList.add("show-code")
+}
